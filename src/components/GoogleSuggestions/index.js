7a344c3259fc7c7d3fcfd16509479bf7a74819dd
Rajashekhar V <rajashekharv.rymec@gmail.com>
2023-03-26 17:10:44 +0000

    Debugged google-search-app ReactJS

---
+++ src/components/GoogleSuggestions/index.js
@@ -0,0 +1,79 @@
+import {Component} from 'react'
+
+import SuggestionItem from '../SuggestionItem'
+
+import './index.css'
+
+// Bugs identified are as follows
+
+// 1. The component is not imported from react
+// 2. The parentheses is used instead object notation in updateSearchInput
+// 3.The event as parameter passed in a wrong way as "e" on onChangeSearchInput
+// 4. while destructuring state used this.props instead this.state
+// 5. while destructuring props used this.state instead this.props
+// 6. suggestion component name starts with lowercase letter
+
+class GoogleSuggestions extends Component {
+  state = {
+    searchInput: '',
+  }
+
+  updateSearchInput = value => {
+    this.setState({searchInput: value})
+  }
+
+  onChangeSearchInput = event => {
+    this.setState({
+      searchInput: event.target.value,
+    })
+  }
+
+  render() {
+    const {searchInput} = this.state
+    const {suggestionsList} = this.props
+    const searchResults = suggestionsList.filter(eachSuggestion =>
+      eachSuggestion.suggestion
+        .toLowerCase()
+        .includes(searchInput.toLowerCase()),
+    )
+
+    return (
+      <div className="app-container">
+        <div className="google-suggestions-container">
+          <img
+            src="https://assets.ccbp.in/frontend/react-js/google-logo.png"
+            alt="google logo"
+            className="google-logo"
+          />
+          <div className="search-input-suggestions-container">
+            <div className="search-input-container">
+              <img
+                alt="search icon"
+                className="search-icon"
+                src="https://assets.ccbp.in/frontend/react-js/google-search-icon.png"
+              />
+              <input
+                type="search"
+                className="search-input"
+                placeholder="Search Google"
+                onChange={this.onChangeSearchInput}
+                value={searchInput}
+              />
+            </div>
+            <ul className="suggestions-list">
+              {searchResults.map(eachSuggestion => (
+                <SuggestionItem
+                  key={eachSuggestion.id}
+                  suggestionDetails={eachSuggestion}
+                  updateSearchInput={this.updateSearchInput}
+                />
+              ))}
+            </ul>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default GoogleSuggestions
